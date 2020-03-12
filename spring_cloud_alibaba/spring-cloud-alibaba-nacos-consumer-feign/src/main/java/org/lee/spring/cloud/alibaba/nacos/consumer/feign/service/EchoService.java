6cf5455c486d9caf2b187a211c6e5518c83bd150
ctrl98 <390022566@qq.com>
2020-03-12 20:27:04 +0800

    整合Sentinel模拟实现熔断

--- spring_cloud_alibaba/spring-cloud-alibaba-nacos-consumer-feign/src/main/java/org/lee/spring/cloud/alibaba/nacos/consumer/feign/service/EchoService.java
+++ spring_cloud_alibaba/spring-cloud-alibaba-nacos-consumer-feign/src/main/java/org/lee/spring/cloud/alibaba/nacos/consumer/feign/service/EchoService.java
@@ -1,10 +1,11 @@
 package org.lee.spring.cloud.alibaba.nacos.consumer.feign.service;
 
+import org.lee.spring.cloud.alibaba.nacos.consumer.feign.service.fallback.EchoServiceFallback;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(value = "nacos-provider")
+@FeignClient(value = "nacos-provider", fallback = EchoServiceFallback.class)
 public interface EchoService {
     @GetMapping(value = "/echo/{message}")
     String echo(@PathVariable("message") String message);
