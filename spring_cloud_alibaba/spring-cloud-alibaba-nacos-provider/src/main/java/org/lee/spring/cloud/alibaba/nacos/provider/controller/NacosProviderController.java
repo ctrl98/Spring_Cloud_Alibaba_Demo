ba9ca17f9d338eff0b2958853f591feb4c889797
ctrl98 <390022566@qq.com>
2020-03-12 20:00:14 +0800

    简单完成feign服务并测试负载均衡

--- spring_cloud_alibaba/spring-cloud-alibaba-nacos-provider/src/main/java/org/lee/spring/cloud/alibaba/nacos/provider/controller/NacosProviderController.java
+++ spring_cloud_alibaba/spring-cloud-alibaba-nacos-provider/src/main/java/org/lee/spring/cloud/alibaba/nacos/provider/controller/NacosProviderController.java
@@ -1,5 +1,6 @@
 package org.lee.spring.cloud.alibaba.nacos.provider.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class NacosProviderController {
 
+    @Value("${server.port}")
+    private String port;
+
     @GetMapping(value = "/echo/{message}")
     public String echo(@PathVariable String message) {
-        return "Hello Nacos Discovery " + message;
+        return "Hello Nacos Discovery " + message + "i am from port " + port;
     }
 }
