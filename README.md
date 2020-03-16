ac6093bd33a2232a8b3629dd00af5ce0be181feb
ctrl98 <390022566@qq.com>
2020-03-16 17:59:18 +0800

    简单完成rocketmq消息队列的订阅

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# Spring_Cloud_Alibaba_Demo
+## Spring Cloud Alibaba Demo
+
+### Spring cloud Alibaba
+
+- Nacos
+- RestTemplate
+- Feign
+- Sentinel
+- Spring Cloud Gateway
+
+### Spring Cloud Alibaba 服务配置
+
+- Nacos Config
+
+### Spring Cloud Alibaba 链路追踪
+
+- SkyWalking
+
+### Spring Cloud Alibaba 异步通讯
+
+- RocketMQ
---
+++ spring_cloud_alibaba/spring-cloud-alibaba-rocketmq-consumer/src/main/java/org/lee/spring/cloud/alibaba/rocketmq/consumer/RocketMQConsumerApplication.java
@@ -0,0 +1,20 @@
+package org.lee.spring.cloud.alibaba.rocketmq.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+
+@SpringBootApplication
+@EnableBinding({Sink.class})
+public class RocketMQConsumerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RocketMQConsumerApplication.class,args);
+    }
+
+    @StreamListener("input")
+    public void receiveInput1(String receiveMsg) {
+        System.out.println("input receive: " + receiveMsg);
+    }
+}
